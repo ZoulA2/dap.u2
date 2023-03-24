ce8a2aa2b932fa05618d668a3c5f306a8080ff64
Z <nicorg.081@gmail.com>
2023-03-24 00:44:02 -0500

    data and video ts made

---
+++ app/vdata.ts
@@ -0,0 +1,24 @@
+interface DataShape {
+    video: string,
+        heart: string,
+        likes: string,
+        comment: string,
+        share: string,
+        profpic: string,
+        profname: string,
+        videsc: string,
+}
+
+const vdata: DataShape[]= [
+    {
+        video: "./videos/Mastodon.mp4",
+        heart: "",
+        likes: "",
+        comment: "",
+        share: "",
+        profpic: "",
+        profname:"",
+        videsc:"",
+
+    },
+]
